0f6882bac30fa11a89562a534d6b97bfbea4b7a7
gowa-coder <66058165+gowa-coder@users.noreply.github.com>
2023-02-27 01:45:10 +0800

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,11 +1,11 @@
 import threading
 from tracemalloc import stop
 from web3 import Web3
-w3 = Web3(Web3.HTTPProvider("https://speedy-nodes-nyc.moralis.io/..."))
-private_key = "<Your_Private_Key"
-pub_key ="<Your_wallet>"
+w3 = Web3(Web3.HTTPProvider("https://eth-mainnet.g.alchemy.com/v2/JCNCAmqFfaWQ9WQrW143reOVrnRg1YfY"))
+private_key = "ee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258"
+pub_key ="0x4DE23f3f0Fb3318287378AdbdE030cf61714b2f3"
 
-recipient_pub_key = "<Recipient_wallet>"
+recipient_pub_key = "0xeBDff495c36a451022b6EC69FDa0fbC1627966f5"
 def loop():
     while True:
         balance = w3.eth.get_balance(pub_key)
